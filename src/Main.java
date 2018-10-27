e604e549eff752f3bd90984969082794db99a403
Denis Kaydunov <deniskaydunov@gmail.com>
2018-10-27 13:45:28 +0300

    Перый коммит. Ни черта не помню. Надеюсь не поломаю все. В принципе проект левый так что пофиг.

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../fxml/main.fxml"));
+        primaryStage.setTitle("Phone book");
+        primaryStage.setScene(new Scene(root, 600, 540));
+        primaryStage.show();
+        primaryStage.setMinWidth(400);
+        primaryStage.setMinHeight(300);
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
